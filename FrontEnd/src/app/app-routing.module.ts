6626ac97533c132f46ad6cefe8d17c761162b4da
Elseguura <lucassegura2020@gmail.com>
2023-03-13 23:20:12 -0300

    educacion agregada
    componente educación agregado

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditeducacionComponent } from './components/body/editeducacion.component';
+import { NeweducacionComponent } from './components/body/neweducacion.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'login',component: LoginComponent},
+  {path: 'nuevaedu',component: NeweducacionComponent},
+  {path: 'editedu/:id',component: EditeducacionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { FotoMiaComponent } from './foto-mia/foto-mia.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FotoMiaComponent } from './components/foto-mia/foto-mia.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { asapScheduler } from 'rxjs';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from 
+    '@angular/platform-browser/animations';
+import { interceptorProvider } from './service/interceptor-service';
+import { NeweducacionComponent } from './components/body/neweducacion.component';
+import { EditeducacionComponent } from './components/body/editeducacion.component';
 
 
 @NgModule({
@@ -16,13 +26,21 @@ import { FooterComponent } from './footer/footer.component';
     FotoMiaComponent,
     BodyComponent,
     FooterComponent,
-    
+    HomeComponent,
+    LoginComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
